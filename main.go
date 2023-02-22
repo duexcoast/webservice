1301c9d7c25b9aa648d035aae1e64bc5dd340cf6
duexcoast <conor.ux@gmail.com>
2023-02-22 13:54:30 -0500

    added makefile commands and vendoring

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 
-	"go.uber.org/automaxprocs"
+	"go.uber.org/automaxprocs/maxprocs"
 )
 
 var build = "develop"
 
 func main() {
-	automaxprocs.New()
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quota
+	if _, err := maxprocs.Set(); err != nil {
+		fmt.Println("maxprocs: %w", err)
+		os.Exit(1)
+	}
+
 	g := runtime.GOMAXPROCS(0)
 
 	log.Printf("starting service build[%s] CPU[%d]", build, g)
