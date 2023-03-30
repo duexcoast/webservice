9e9abce31f685e4fb13795aad65e7f17a5d26d94
duexcoast <conor.ux@gmail.com>
2023-03-30 18:51:25 -0400

    built tooling for generating private and public keys.

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -62,6 +62,9 @@ func APIMux(cfg APIMuxConfig) *web.App {
 	app := web.NewApp(
 		cfg.Shutdown,
 		mid.Logger(cfg.Log),
+		mid.Errors(cfg.Log),
+		mid.Metrics(),
+		mid.Panics(),
 	)
 
 	// Load the routes for teh different versions of the API.
