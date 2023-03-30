a42bfc31cdea86c7a227aa55340f6f88721c4001
duexcoast <conor.ux@gmail.com>
2023-03-30 12:08:16 -0400

    foundation layer shutdownError type created

--- business/web/mid/logger.go
+++ business/web/mid/logger.go
@@ -9,11 +9,14 @@ import (
 	"go.uber.org/zap"
 )
 
-// Logger...
+// Logger writes some information about the request to the logs in the
+// format: TraceID : (200) GET /foo -> IP ADDR (latency)
 func Logger(log *zap.SugaredLogger) web.Middleware {
 
+	// This is the actual middleware function to be executed.
 	m := func(handler web.Handler) web.Handler {
 
+		// Create the handler that will be attached in the middleware chain.
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
 			// If the context is missing this value, request the service
